8a136ddf42d9f29412435d4bc1063ef370105809
Kim Rutherford <kmr@aska.gen.nz>
2022-09-04 18:57:33 +1200

    Improve config file checking script and action
    Currently only checks that "date_added" and "head_image" are sensible.
    
    Refs pombase/website#1908

--- etc/check_config.pl
+++ etc/check_config.pl
@@ -8,9 +8,12 @@ use Text::CSV;
 use JSON;
 
 
+my $config_filename = shift;
+my $metadata_filename = shift;
+my $assets_dir = shift;
+
 my $csv = Text::CSV->new({ blank_is_undef => 1, binary => 1, auto_diag => 1  });
 
-my $metadata_filename = 'website/jbrowse_track_metadata.csv';
 
 open my $metadata_fh, '<', $metadata_filename
   or die "can't open metadata file $!";
@@ -27,29 +30,58 @@ while (my $row = $csv->getline($metadata_fh)) {
 close $metadata_fh;
 
 
-
-my $config_filename = 'website/pombase_v2_config.json';
-
-open my $config_fh, '<', $config_filename;
-
-$/ = undef;
-
-my $config_text = <$config_fh>;
+open my $config_fh, '<', $config_filename or
+  die "can't open $config_filename\n";
+my $config_text = undef;
+{
+  $/ = undef;
+  $config_text = <$config_fh>;
+}
+close $config_fh;
 
 my $config = decode_json $config_text;
 
 my $panels = $config->{front_page_panels};
 
 map {
-  my @head_images = @{$_->{head_image}};
+  my $panel = $_;
 
-  map {
-    my $image_filename = $_;
+  if (defined $panel->{head_image}) {
+    my @head_images = @{$panel->{head_image}};
+    map {
+      my $head_image = $_;
+      if ($head_image !~ m|^/media/|) {
+        if ($head_image =~ m|^/|) {
+          die qq|"head_image" shouldn't start with "/": $head_image\n|;
+        }
+        if (! -f "$assets_dir/$head_image") {
+          die qq|missing file for "head_image": $head_image\n|;
+        }
+      }
+    } @head_images;
+  } else {
+    die 'missing "head_image" for: ', Dumper([$panel]);
+  }
 
-    if ($image_filename !~ m|^/| && !-f "pombase-website/src/assets/$image_filename") {
-      die "can't find image from config file in the website repository: $image_filename\n";
+  my $date_added = $panel->{date_added};
+
+  if (defined $date_added) {
+    if ($date_added =~ /^(\d\d\d\d)-(\d\d)-(\d\d)/) {
+      if ($1 < 2015) {
+        die qq|year too far in the past for "date_added": "$date_added"\n|;
+      }
+      if ($2 > 12) {
+        die qq|month should be 1-12 for "date_added": "$date_added"\n|;
+      }
+      if ($3 > 31) {
+        die qq|day of month should be 1-31 for "date_added": "$date_added"\n|;
+      }
+   } else {
+      die "unknown date format: $date_added\n";
     }
-  } @head_images;
+  } else {
+    die 'missing "date_added" for: ', Dumper([$panel]);
+  }
 } @$panels;
 
-close $config_fh;
+
